73d94c59646194776f3a06f01b43e0b4d4763335
ttwag <taowang@ucdavis.edu>
2024-01-10 19:23:56 -0800

    Update tb_partII.v

--- lab1/test/tb_partII/tb_partII.v
+++ lab1/test/tb_partII/tb_partII.v
@@ -6,14 +6,13 @@ module tb_partII;
 	wire [7:0] HEX3;
 	wire [7:0] HEX4;
 	wire [7:0] HEX5;
-	wire [1:0] KEY;
 	wire [9:0] LEDR;
 	wire [9:0] SW;
 
 	assign SW[3:0] = count;
 	partII UUT (.HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2),
 		.HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5),
-		.KEY(KEY), .LEDR(LEDR), .SW(SW));
+		.LEDR(LEDR), .SW(SW));
 	initial begin
 	  count = 4'b0000;
 	  repeat (16) begin
