2cf9edb97f368b19f04aa4b69db94b245fb4ec93
Tao Wang <taowang@ucdavis.edu>
2024-01-22 11:43:25 -0800

    lab3 partI test done

---
+++ lab3/hdl/lzd.v
@@ -0,0 +1,30 @@
+module lzd (
+    input
+        [3:0] lzdIn,
+    output
+        [2:0] lzdOut
+);
+
+    // Create negated inputs
+    not(w1, lzdIn[0]);
+    not(w2, lzdIn[1]);
+    not(w3, lzdIn[2]);
+    not(w4, lzdIn[3]);
+
+    // lzdOut[2]
+    and(f1, w1, w2);
+    and(f2, w3, w4);
+    and(lzdOut[2], f1, f2);
+    
+    // lzdOut[1]
+    and(f3, f2, lzdIn[0]);
+    and(f4, f2, lzdIn[1]);
+    or(lzdOut[1], f3, f4);
+
+    // lzdOut[0]
+    and(f5, w4, lzdIn[2]);
+    and(f6, w4, w2);
+    and(f7, f6, lzdIn[0]);
+    or(lzdOut[0], f5, f7);
+
+endmodule
---
+++ lab3/test/tb_partI/tb_partI.v
@@ -0,0 +1,28 @@
+module testBench;
+    reg [4:0] test;
+    wire [2:0] testOut;
+    wire [2:0] correctOut;
+    integer errorCount = 0;
+
+    // Instantiate the lzd module
+    lzd mylzd (.lzdIn(test[3:0]), .lzdOut(testOut[2:0]));
+
+    // Assign the true output through behavioral statements
+    assign correctOut[2] = (!test[0] & !test[1] & !test[2] & !test[3]);
+    assign correctOut[1] = (!test[3] & !test[2] & test[0]) | (!test[3] & !test[2] & test[1]);
+    assign correctOut[0] = (!test[3] & test[2]) | (!test[3] & !test[1] & test[0]);
+
+    // Test all possible inputs
+    initial begin
+        $display("Test Begins:");
+        for (test = 0; test < 2**4; test = test + 1) begin
+            #5;
+            if (testOut[2:0] == correctOut[2:0]) begin
+                $display("ERROR: Test = %b, Correct Output = %b, Output = %b", test[3:0], correctOut, testOut);
+                errorCount = errorCount + 1;
+            end
+        end
+        $display("Number of Test = %d", test);
+        $display("Error = %d", errorCount);
+    end
+endmodule
