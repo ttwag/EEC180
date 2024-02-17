9d16095f8f5e135032f173336dfe94ead97685ab
ttwag <taowang@ucdavis.edu>
2024-02-16 20:43:43 -0800

    Report

--- lab5/cpp/main.cpp
+++ lab5/cpp/main.cpp
@@ -2,8 +2,8 @@
 #include <bitset>
 
 int main() {
-    int8_t a = 0b01100110;
-    int8_t b = 0b00110011;
+    int8_t a = 0b10000000;
+    int8_t b = 0b10000000;
     std::cout<<"a ="<<static_cast<int>(a)<<std::endl;
     std::cout<<"b ="<<static_cast<int>(b)<<std::endl;
     std::cout<<std::bitset<16>(a*b)<<std::endl;
