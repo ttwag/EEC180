f94de31a1fd0c05ab2c7ff46bc8ac5c74183b9b3
Tao Wang <taowang@ucdavis.edu>
2024-03-04 11:43:22 -0800

    lab 6 P1

--- lab6/hdl/controller.v
+++ lab6/hdl/controller.v
@@ -14,7 +14,7 @@ localparam Sread = 2'b01;
 localparam Swrite = 2'b10;
 
 reg [1:0] state, nextState;
-reg [8:0] dim_x, dim_y, dim_x_r, dim_y_r;
+//reg [8:0] dim_x, dim_y, dim_x_r, dim_y_r;
 reg [15:0] addr_r;
 reg start_r;
 
@@ -23,9 +23,9 @@ always@(posedge clk or negedge rst_n) begin
     else begin 
         state <= nextState;
         addr_r <= addr;
-        dim_x_r <= dim_x;
-        dim_y_r <= dim_y;
-        start_r <= start;
+        //dim_x_r <= dim_x;
+        //dim_y_r <= dim_y;
+        start <= start_r;
     end
 end
 
@@ -33,56 +33,62 @@ always@(*) begin
     case(state)
         Sidle:begin
             // Reset state
-            start = start_r;
+            start_r = 1'b0;
             addr = 16'b0;
             datain = 8'b0;
             writeEnable = 1'b0;
-            dim_x = 9'b1;
-            dim_y = 9'b1;
+            //dim_x = 9'b1;
+            //dim_y = 9'b1;
             nextState = Sidle;
-            if (ready == 1'b0) begin
-                start = 1'b0;
-                nextState = Sread;
+            if (ready == 1'b1 && start != 1'b1) begin
+                nextState = Swrite;
             end
         end
         Sread:begin
             // Read Memory State
-            start = 1'b0;
+            start_r = 1'b0;
             addr = addr_r + 16'b1;
-            datain = 8'b0;
+            datain = 8'b1;
             writeEnable = 1'b0;
             dim_x = dim_x_r;
             dim_y = dim_y_r;
             nextState = Swrite;
         end
+
         Swrite:begin
             // Write Back State
-            start = 1'b0;
-            //addr = addr_r + 16'b1;
+            start_r = 1'b0;
             addr = addr_r;
             datain = dataout;
             writeEnable = 1'b1;
-            if (dim_x >= dim && dim_y >= dim) begin
-                // Go back to Sidle when finished
-                start = 1'b1;
-                dim_x = 9'b1;
-                dim_y = 9'b1;
+            if (addr >= (dim * dim - 1)) begin 
+                start_r = 1'b1;
                 nextState = Sidle;
             end
-            else if (dim_x >= dim) begin
-                // We are at the bottom of the row. 
-                // Go back Sread and to the next column
-                dim_x = 9'b1;
-                dim_y = dim_y_r + 9'b1;
-                nextState = Sread;
-            end
             else begin
-                // We are NOT at the bottom of the row.
-                // Go back to Sread stay at the same column
-                dim_x = dim_x_r + 9'b1;
-                dim_y = dim_y_r;
                 nextState = Sread;
             end
+            // if ((dim_x >= (dim - 9'b1)) && (dim_y >= (dim - 9'b1))) begin
+            //     // Go back to Sidle when finished
+            //     start_r = 1'b1;
+            //     dim_x = 9'b0;
+            //     dim_y = 9'b0;
+            //     nextState = Sidle;
+            // end
+            // else if (dim_x >= (dim - 9'b1)) begin
+            //     // We are at the bottom of the row. 
+            //     // Go back Sread and to the next column
+            //     dim_x = 9'b1;
+            //     dim_y = dim_y_r + 9'b1;
+            //     nextState = Sread;
+            // end
+            // else begin
+            //     // We are NOT at the bottom of the row.
+            //     // Go back to Sread stay at the same column
+            //     dim_x = dim_x_r + 9'b1;
+            //     dim_y = dim_y_r;
+            //     nextState = Sread;
+            // end
         end
     endcase
 end
