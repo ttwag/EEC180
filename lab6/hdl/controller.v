3b8329363c590761774c6592bdd9dbcf1eafcffc
Tao Wang <taowang@ucdavis.edu>
2024-03-06 09:41:55 -0800

    lab 6 part 1 ready

--- lab6/hdl/controller.v
+++ lab6/hdl/controller.v
@@ -14,7 +14,6 @@ localparam Sread = 2'b01;
 localparam Swrite = 2'b10;
 
 reg [1:0] state, nextState;
-//reg [8:0] dim_x, dim_y, dim_x_r, dim_y_r;
 reg [15:0] addr_r;
 reg start_r;
 
@@ -23,8 +22,6 @@ always@(posedge clk or negedge rst_n) begin
     else begin 
         state <= nextState;
         addr_r <= addr;
-        //dim_x_r <= dim_x;
-        //dim_y_r <= dim_y;
         start <= start_r;
     end
 end
@@ -37,8 +34,6 @@ always@(*) begin
             addr = 16'b0;
             datain = 8'b0;
             writeEnable = 1'b0;
-            //dim_x = 9'b1;
-            //dim_y = 9'b1;
             nextState = Sidle;
             if (ready == 1'b1 && start != 1'b1) begin
                 nextState = Swrite;
@@ -50,8 +45,6 @@ always@(*) begin
             addr = addr_r + 16'b1;
             datain = 8'b1;
             writeEnable = 1'b0;
-            // dim_x = dim_x_r;
-            // dim_y = dim_y_r;
             nextState = Swrite;
         end
 
@@ -68,27 +61,6 @@ always@(*) begin
             else begin
                 nextState = Sread;
             end
-            // if ((dim_x >= (dim - 9'b1)) && (dim_y >= (dim - 9'b1))) begin
-            //     // Go back to Sidle when finished
-            //     start_r = 1'b1;
-            //     dim_x = 9'b0;
-            //     dim_y = 9'b0;
-            //     nextState = Sidle;
-            // end
-            // else if (dim_x >= (dim - 9'b1)) begin
-            //     // We are at the bottom of the row. 
-            //     // Go back Sread and to the next column
-            //     dim_x = 9'b1;
-            //     dim_y = dim_y_r + 9'b1;
-            //     nextState = Sread;
-            // end
-            // else begin
-            //     // We are NOT at the bottom of the row.
-            //     // Go back to Sread stay at the same column
-            //     dim_x = dim_x_r + 9'b1;
-            //     dim_y = dim_y_r;
-            //     nextState = Sread;
-            // end
         end
     endcase
 end
